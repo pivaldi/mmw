1b9c319d91c5ae37c4f4600cf12722f4b5cb1317
pivaldi <git@ivaldi.me>
2026-04-02 19:05:03 +0200

    Add E2E tests in the monlith

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ tool (
 )
 
 require (
+	connectrpc.com/connect v1.19.1
 	github.com/ThreeDotsLabs/watermill v1.5.1
 	github.com/jackc/pgx/v5 v5.9.1
 	github.com/piprim/mmw v0.0.0-20260330215535-bc0f2aacc35b
@@ -16,53 +17,97 @@ require (
 	github.com/pivaldi/mmw-notifications v0.0.0-20260326101319-caf95dfd2c9e
 	github.com/pivaldi/mmw-todo v0.0.0-20260321181339-5d9491538aa7
 	github.com/rotisserie/eris v0.5.4
+	github.com/stretchr/testify v1.11.1
+	github.com/testcontainers/testcontainers-go v0.41.0
+	github.com/testcontainers/testcontainers-go/modules/postgres v0.41.0
 )
 
 require (
-	connectrpc.com/connect v1.19.1 // indirect
 	connectrpc.com/cors v0.1.0 // indirect
 	dario.cat/mergo v1.0.2 // indirect
+	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/RocketChat/Rocket.Chat.Go.SDK v0.0.0-20250718055228-285ecf400b48 // indirect
 	github.com/air-verse/air v1.64.5 // indirect
 	github.com/andybalholm/brotli v1.2.0 // indirect
 	github.com/bep/godartsass/v2 v2.5.0 // indirect
 	github.com/bep/golibsass v1.2.0 // indirect
 	github.com/bitfield/gotestdox v0.2.2 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/containerd/errdefs v1.0.0 // indirect
+	github.com/containerd/errdefs/pkg v0.3.0 // indirect
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/containerd/platforms v0.2.1 // indirect
+	github.com/cpuguy83/dockercfg v0.3.2 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/distribution/reference v0.6.0 // indirect
 	github.com/dnephin/pflag v1.0.7 // indirect
+	github.com/docker/docker v28.5.2+incompatible // indirect
+	github.com/docker/go-connections v0.6.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/ebitengine/purego v0.10.0 // indirect
 	github.com/fatih/color v1.18.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-viper/mapstructure/v2 v2.4.0 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gohugoio/hugo v0.149.1 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.18.4 // indirect
 	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
 	github.com/lmittmann/tint v1.1.3 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/magiconair/properties v1.8.10 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/mickamy/goimportmaps v0.2.0 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/moby/go-archive v0.2.0 // indirect
+	github.com/moby/patternmatcher v0.6.0 // indirect
+	github.com/moby/sys/sequential v0.6.0 // indirect
+	github.com/moby/sys/user v0.4.0 // indirect
+	github.com/moby/sys/userns v0.1.0 // indirect
+	github.com/moby/term v0.5.2 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/nikoksr/notify v1.5.0 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.1 // indirect
+	github.com/ovya/ogl v0.0.0-20260324224052-968c2f821ca3 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/power-devops/perfstat v0.0.0-20240221224432-82ca36839d55 // indirect
 	github.com/rs/cors v1.11.1 // indirect
 	github.com/sagikazarmark/locafero v0.11.0 // indirect
 	github.com/sethvargo/go-envconfig v1.3.0 // indirect
+	github.com/shirou/gopsutil/v4 v4.26.2 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/sourcegraph/conc v0.3.1-0.20240121214520-5f936abd7ae8 // indirect
 	github.com/spf13/afero v1.15.0 // indirect
 	github.com/spf13/cast v1.10.0 // indirect
-	github.com/spf13/cobra v1.10.2 // indirect
 	github.com/spf13/pflag v1.0.10 // indirect
 	github.com/spf13/viper v1.21.0 // indirect
 	github.com/subosito/gotenv v1.6.0 // indirect
 	github.com/tdewolff/parse/v2 v2.8.3 // indirect
+	github.com/tklauser/go-sysconf v0.3.16 // indirect
+	github.com/tklauser/numcpus v0.11.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.4 // indirect
+	go.opentelemetry.io/auto/sdk v1.2.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.65.0 // indirect
+	go.opentelemetry.io/otel v1.41.0 // indirect
+	go.opentelemetry.io/otel/metric v1.41.0 // indirect
+	go.opentelemetry.io/otel/trace v1.41.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
 	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/mod v0.32.0 // indirect
---
+++ test/system/todo_flow_test.go
@@ -0,0 +1,257 @@
+//go:build system
+
+package system
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/ThreeDotsLabs/watermill"
+	"github.com/ThreeDotsLabs/watermill/pubsub/gochannel"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/testcontainers/testcontainers-go"
+	tcpostgres "github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+
+	"connectrpc.com/connect"
+	authdef "github.com/pivaldi/mmw-contracts/definitions/auth"
+	authv1 "github.com/pivaldi/mmw-contracts/gen/go/auth/v1"
+	"github.com/pivaldi/mmw-contracts/gen/go/auth/v1/authv1connect"
+	todov1 "github.com/pivaldi/mmw-contracts/gen/go/todo/v1"
+	"github.com/pivaldi/mmw-contracts/gen/go/todo/v1/todov1connect"
+	pfevents "github.com/piprim/mmw/pkg/platform/events"
+	auth "github.com/pivaldi/mmw-auth"
+	todo "github.com/pivaldi/mmw-todo"
+)
+
+var (
+	authServer *httptest.Server
+	todoServer *httptest.Server
+)
+
+func TestMain(m *testing.M) {
+	ctx := context.Background()
+
+	// Required by auth.New() and todo.New() config.Load() validation.
+	os.Setenv("APP_ENV", "development")
+	os.Setenv("APP_NAME", "test")
+	os.Setenv("JWT_SECRET", "fake-secret")
+	os.Setenv("DB_PASSWORD", "postgres")
+
+	pgContainer, err := tcpostgres.Run(ctx,
+		"postgres:18-alpine",
+		tcpostgres.WithDatabase("mmw"),
+		tcpostgres.WithUsername("postgres"),
+		tcpostgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").WithOccurrence(2),
+		),
+	)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to start postgres: %v\n", err)
+		os.Exit(1)
+	}
+	defer func() { _ = pgContainer.Terminate(ctx) }()
+
+	connStr, err := pgContainer.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to get connection string: %v\n", err)
+		os.Exit(1)
+	}
+
+	pool, err := pgxpool.New(ctx, connStr)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer pool.Close()
+
+	if err := auth.Migrate(ctx, pool); err != nil {
+		fmt.Fprintf(os.Stderr, "auth migration failed: %v\n", err)
+		os.Exit(1)
+	}
+	if err := todo.Migrate(ctx, pool); err != nil {
+		fmt.Fprintf(os.Stderr, "todo migration failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError}))
+	rawBus := gochannel.NewGoChannel(
+		gochannel.Config{OutputChannelBuffer: 64, Persistent: false},
+		watermill.NewSlogLogger(logger),
+	)
+	defer rawBus.Close()
+	systemBus := pfevents.NewWatermillBus(rawBus)
+
+	authModule, err := auth.New(auth.Infrastructure{
+		DBPool:   pool,
+		EventBus: systemBus,
+		Logger:   logger.With("module", auth.ModuleName),
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create auth module: %v\n", err)
+		os.Exit(1)
+	}
+
+	todoModule, err := todo.New(todo.Infrastructure{
+		DBPool:   pool,
+		EventBus: systemBus,
+		Logger:   logger.With("module", todo.ModuleName),
+		AuthSvc:  authdef.NewInprocClient(authModule.Service()),
+	})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to create todo module: %v\n", err)
+		os.Exit(1)
+	}
+
+	authServer = httptest.NewServer(authModule.Handler())
+	defer authServer.Close()
+	todoServer = httptest.NewServer(todoModule.Handler())
+	defer todoServer.Close()
+
+	os.Exit(m.Run())
+}
+
+func newAuthClient() authv1connect.AuthServiceClient {
+	return authv1connect.NewAuthServiceClient(http.DefaultClient, authServer.URL)
+}
+
+func newTodoClient(token string) todov1connect.TodoServiceClient {
+	return todov1connect.NewTodoServiceClient(
+		http.DefaultClient,
+		todoServer.URL,
+		connect.WithInterceptors(bearerInterceptor(token)),
+	)
+}
+
+func bearerInterceptor(token string) connect.UnaryInterceptorFunc {
+	return func(next connect.UnaryFunc) connect.UnaryFunc {
+		return func(ctx context.Context, req connect.AnyRequest) (connect.AnyResponse, error) {
+			req.Header().Set("Authorization", "Bearer "+token)
+			return next(ctx, req)
+		}
+	}
+}
+
+func registerAndLogin(t *testing.T, login, password string) string {
+	t.Helper()
+	ctx := context.Background()
+	ac := newAuthClient()
+
+	_, err := ac.Register(ctx, connect.NewRequest(&authv1.RegisterRequest{
+		Login:    login,
+		Password: password,
+	}))
+	require.NoError(t, err)
+
+	loginResp, err := ac.Login(ctx, connect.NewRequest(&authv1.LoginRequest{
+		Login:    login,
+		Password: password,
+	}))
+	require.NoError(t, err)
+	return loginResp.Msg.GetToken()
+}
+
+func TestSystem_Unauthenticated(t *testing.T) {
+	tc := todov1connect.NewTodoServiceClient(http.DefaultClient, todoServer.URL)
+	_, err := tc.ListTodos(context.Background(), connect.NewRequest(&todov1.ListTodosRequest{}))
+	require.Error(t, err)
+	var connectErr *connect.Error
+	require.ErrorAs(t, err, &connectErr)
+	assert.Equal(t, connect.CodeUnauthenticated, connectErr.Code())
+}
+
+func TestSystem_TodoCRUDFlow(t *testing.T) {
+	token := registerAndLogin(t, "crud@test.com", "password123")
+	tc := newTodoClient(token)
+	ctx := context.Background()
+
+	createResp, err := tc.CreateTodo(ctx, connect.NewRequest(&todov1.CreateTodoRequest{
+		Title:       "Buy milk",
+		Description: "Full fat",
+		Priority:    todov1.Priority_PRIORITY_MEDIUM,
+	}))
+	require.NoError(t, err)
+	id := createResp.Msg.Todo.Id
+	assert.NotEmpty(t, id)
+	assert.Equal(t, "Buy milk", createResp.Msg.Todo.Title)
+	assert.Equal(t, todov1.TaskStatus_TASK_STATUS_PENDING, createResp.Msg.Todo.Status)
+
+	getResp, err := tc.GetTodo(ctx, connect.NewRequest(&todov1.GetTodoRequest{Id: id}))
+	require.NoError(t, err)
+	assert.Equal(t, "Buy milk", getResp.Msg.Todo.Title)
+
+	newTitle := "Buy oat milk"
+	updateResp, err := tc.UpdateTodo(ctx, connect.NewRequest(&todov1.UpdateTodoRequest{
+		Id:    id,
+		Title: &newTitle,
+	}))
+	require.NoError(t, err)
+	assert.Equal(t, "Buy oat milk", updateResp.Msg.Todo.Title)
+
+	completeResp, err := tc.CompleteTodo(ctx, connect.NewRequest(&todov1.CompleteTodoRequest{Id: id}))
+	require.NoError(t, err)
+	assert.Equal(t, todov1.TaskStatus_TASK_STATUS_COMPLETED, completeResp.Msg.Todo.Status)
+
+	reopenResp, err := tc.ReopenTodo(ctx, connect.NewRequest(&todov1.ReopenTodoRequest{Id: id}))
+	require.NoError(t, err)
+	assert.Equal(t, todov1.TaskStatus_TASK_STATUS_PENDING, reopenResp.Msg.Todo.Status)
+
+	_, err = tc.DeleteTodo(ctx, connect.NewRequest(&todov1.DeleteTodoRequest{Id: id}))
+	require.NoError(t, err)
+
+	_, err = tc.GetTodo(ctx, connect.NewRequest(&todov1.GetTodoRequest{Id: id}))
+	require.Error(t, err)
+	var connectErr *connect.Error
+	require.ErrorAs(t, err, &connectErr)
+	assert.Equal(t, connect.CodeNotFound, connectErr.Code())
+}
+
+func TestSystem_ListTodos_StatusFilter(t *testing.T) {
+	token := registerAndLogin(t, "listfilter@test.com", "password123")
+	tc := newTodoClient(token)
+	ctx := context.Background()
+
+	for _, title := range []string{"Pending A", "Pending B"} {
+		_, err := tc.CreateTodo(ctx, connect.NewRequest(&todov1.CreateTodoRequest{
+			Title:    title,
+			Priority: todov1.Priority_PRIORITY_MEDIUM,
+		}))
+		require.NoError(t, err)
+	}
+	doneResp, err := tc.CreateTodo(ctx, connect.NewRequest(&todov1.CreateTodoRequest{
+		Title:    "Done C",
+		Priority: todov1.Priority_PRIORITY_LOW,
+	}))
+	require.NoError(t, err)
+	_, err = tc.CompleteTodo(ctx, connect.NewRequest(&todov1.CompleteTodoRequest{Id: doneResp.Msg.Todo.Id}))
+	require.NoError(t, err)
+
+	status := todov1.TaskStatus_TASK_STATUS_PENDING
+	listResp, err := tc.ListTodos(ctx, connect.NewRequest(&todov1.ListTodosRequest{Status: &status}))
+	require.NoError(t, err)
+	assert.Equal(t, 2, len(listResp.Msg.Todos))
+}
+
+func TestSystem_TodoScopedToUser(t *testing.T) {
+	tokenA := registerAndLogin(t, "usera@test.com", "passwordA")
+	tokenB := registerAndLogin(t, "userb@test.com", "passwordB")
+	ctx := context.Background()
+
+	_, err := newTodoClient(tokenA).CreateTodo(ctx, connect.NewRequest(&todov1.CreateTodoRequest{
+		Title:    "User A secret",
+		Priority: todov1.Priority_PRIORITY_HIGH,
+	}))
+	require.NoError(t, err)
+
+	listResp, err := newTodoClient(tokenB).ListTodos(ctx, connect.NewRequest(&todov1.ListTodosRequest{}))
+	require.NoError(t, err)
+	assert.Empty(t, listResp.Msg.Todos)
+}
